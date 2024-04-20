d342994978fb0d75e3826ff9ef52dc2982f923ad
Salvador Rodriguez <chava.crb@gmail.com>
2024-04-20 15:52:41 -0600

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
-import express from 'express';
-import colors from 'colors';
+import express from "express";
+import colors from "colors";
+import connectDB from './config/db.js';
+import dotenv from "dotenv";
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000 ...'.yellow.bold);
-})
+  console.log("Server is running on port 3000 ...".yellow.bold);
+});
