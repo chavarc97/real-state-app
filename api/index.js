8224e455dd69435731039ba353c7df0615c978ee
Salvador Rodriguez <chava.crb@gmail.com>
2024-04-20 18:00:09 -0600

    create signUp api route

--- api/index.js
+++ api/index.js
@@ -3,11 +3,20 @@ import colors from "colors";
 import connectDB from './config/db.js';
 import dotenv from "dotenv";
 dotenv.config();
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000 ...".yellow.bold);
 });
+
+// routes 
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
