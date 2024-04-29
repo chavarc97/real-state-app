3b93e2f505f15140d899b72acf2a7f4245b993fb
Salvador Rodriguez <chava.crb@gmail.com>
2024-04-28 21:33:32 -0600

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,31 @@
 const About = () => {
   return (
     <>
-   
+      <main className="py-20 px-4 max-w-6xl mx-auto">
+        <h1 className="text-4xl font-bold mb-4 text-sky-800">
+          About NestQuest
+        </h1>
+        <p className="mb-4 text-neutral-600 text-justify">
+          At NestQuest, we're a premier real estate agency specializing in
+          helping clients buy, sell, and rent properties in sought-after
+          neighborhoods. Our seasoned team of agents is committed to delivering
+          exceptional service, ensuring a seamless experience throughout the
+          buying and selling journey
+        </p>
+        <p className="mb-4 text-neutral-600 text-justify">
+          Our mission is to empower clients to achieve their real estate dreams
+          with expert advice, personalized attention, and a deep knowledge of
+          the local market. Whether you're seeking to buy, sell, or rent, we're
+          dedicated to guiding you every step of the way.
+        </p>
+        <p className="mb-4 text-neutral-600 text-justify">
+          Backed by extensive industry experience, our agents are focused on
+          providing top-tier service to our clients. We believe that buying or
+          selling a property should be a thrilling and fulfilling endeavor, and
+          we're here to turn that vision into reality for each client we serve.
+        </p>
+      </main>
     </>
-  )
-}
-export default About
+  );
+};
+export default About;
